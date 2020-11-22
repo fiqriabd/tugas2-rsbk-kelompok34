b6d00db27aab87b2e6e580d3420aa84864f0a987
fiqriabd <fiqriabd1999@gmail.com>
2020-11-22 20:00:30 +0700

    update README

---
+++ src/Component/Home/Home.jsx
@@ -0,0 +1,18 @@
+import React, {} from 'react';
+import Cardinfo from '../Cardinfo/Cardinfo';
+import Heading from '../Heading/Heading';
+
+export default function Home(){
+    return(
+        <div>
+            <div>
+                <Heading/>
+            </div>
+            <div>
+                <Cardinfo />
+            </div>
+        </div>
+       
+    )
+}
+
